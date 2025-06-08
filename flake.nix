ad3ee255b689639136da52ddbdba73cc24c9df36
Giannin <contact@giannin.ch>
2025-06-08 20:34:09 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Development flake for nocodb-reminder-emails";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs =
